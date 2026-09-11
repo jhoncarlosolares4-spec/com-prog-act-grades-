be523d94ddda099c0fb659714973a2a383c5925c
jhoncarlosolares4-spec <jhoncarlosolares4@gmail.com>
2026-09-11 11:34:51 +0800

    Add files via upload

---
+++ index jc.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std; 
+ int main() {   
+      int grade;  
+   cout << "Enter your grade; "; 
+   cin >> grade;     
+
+ if (grade >= 90) {      
+       cout << "Excellent"; 
+
+ } else if (grade >= 85) {
+    cout << "VeryGood";
+
+ } else if (grade >= 75) {
+    cout << "Passed";
+
+ } else {
+    cout << "Failed";
+ 
+ }
+ }
